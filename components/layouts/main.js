be57921263f72457295c2391a58bbd329b0a5185
DmitriyBakhanenko <bahanenko@gmail.com>
2022-05-17 23:06:14 +0300

    three model added and configured

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
+import VoxelCatLoader from '../voxel-cat-loader'
+import dynamic from 'next/dynamic'
+
+const LazyVoxelCat = dynamic(() => import('../voxelcat'), {
+  ssr: false,
+  loading: () => <VoxelCatLoader />
+})
 
 const Main = ({ children, router }) => {
 
@@ -10,10 +17,9 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Bakhanenko Dmitriy Website</title>
       </Head>
-
       <Navbar path={router.asPath} />
-
-      <Container maxW="container.md" pt={4} >
+      <Container maxW="container.md" pt={14} >
+        <LazyVoxelCat />
         {children}
       </Container>
     </Box>
