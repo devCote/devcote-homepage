451fb69d330976234b888e8701f2e9b7dda06972
DmitriyBakhanenko <bahanenko@gmail.com>
2022-05-23 23:08:07 +0300

    change index

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Page = () => {
         bg={colorMode === 'dark' ? '#303030' : '#fbd38d'}
         p={3} mb={6} align="center" mt={'80px'}>
         {lang ? `Hello! I${apos}m a full-stack developer from Ukraine`
-          : 'Вітаю! Я веб розробник з України'}
+          : 'Привет! Я веб разработчик из Украины'}
       </Box>
       <Box display={{ md: 'flex' }} mb={{ base: 4, md: 0 }}>
         <Box flexGrow={1} mb={6}>
@@ -24,7 +24,7 @@ const Page = () => {
             Dmytro Bakhanenko
           </Heading>
           <Text mt={{ base: '0', sm: '-4' }} fontWeight='bold' align='right'>
-            {lang ? "devCote ( Artist / Designer / Developer )" : "devCote ( Митець / Дизайнер / Програміст )"}
+            {lang ? "devCote ( Artist / Designer / Developer )" : "devCote ( Художник / Дизайнер / Програмист )"}
           </Text>
         </Box>
         <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }}
@@ -37,15 +37,25 @@ const Page = () => {
       </Box>
       <Section delay={0.2} >
         <Heading as={'h3'} mt={{ base: '-40px', md: '0' }} variant='section-title'>
-          About
+          {lang ? 'About' : 'Обо мне'}
         </Heading>
         <Text px={10}>
-          {lang ? 'Wow, the cool kids are already coding at age 7. Meanwhile, I started at age 25. I heard that electronics engineering had programming subjects, so I decided to study it in advance. My first language was C++, and I must say that I quite got the basics of programming. I learned the syntax, operations, looping, conditionals, and little of object-oriented programming.' : "Вау, круті діти вже кодують у віці 7 років. Тим часом я почав у віці 25 років. Я чув, що електроніка техніки має предмети програмування, тому я вирішив вивчити його заздалегідь. Моєю першою мовою був C++, і я повинен сказати, що я цілком отримав основи програмування. Я вивчив синтаксис, операції, петлі, умовні умови та мало об'єктно-орієнтованого програмування."}
+          {lang ?
+            `Dmitriy is a freelance and full-stack developer from Kyiv Ukraine with a
+            passion of building apps and services/stuff he likes. He has a stack
+            of all the things for launching products, from planning and designing all the
+            way to solving problems with code. When not online, he loves playing guitar, travelling and
+            studying cyber security tech`
+
+            : `Дмитрий - фрилансер и разработчик полного стека из Киева Украина. Имеет страсть к созданию приложений
+            услуг и вещей, которые ему нравятся. У него есть знания для запуска продуктов, от планирования
+            и проектирования до способов решения проблем с кодом. Когда он не онлайн, он любит играть на гитаре,
+            путешествовать и изучать технологит кибербезопасности`}
         </Text>
       </Section>
       <Section delay={0.6} >
         <Heading as={'h3'} variant='section-title'>
-          Tech
+          {lang ? 'Tech' : 'Технологии'}
         </Heading>
         <Box align='center'>
           <Ireact boxSize={12} fill={'currentColor'} m={2} />
@@ -66,25 +76,25 @@ const Page = () => {
           <Iredis boxSize={12} fill={'currentColor'} m={2} />
           <Inode boxSize={12} fill={'currentColor'} m={2} />
           <Iblender boxSize={12} fill={'currentColor'} m={2} />
-          <Iadobe boxSize={12} fill={'currentColor'} m={2} />
           <Iunity boxSize={12} fill={'currentColor'} m={2} />
           <Ibash boxSize={12} fill={'currentColor'} m={2} />
           <Ilinux boxSize={12} fill={'#333'} m={2} />
           <Ikubernetes boxSize={12} fill={'currentColor'} m={2} />
-          <Ivim boxSize={12} fill={'currentColor'} m={2} />
         </Box>
       </Section>
-      <Section delay={0.2} >
-        <Heading as={'h3'} mt={4} variant='section-title'>
-          Works
-        </Heading>
-        <Box mx={10}>
 
-          <Text>Lorem ipsum dolor sit amet</Text>
-          <Text>onsectetur adipiscing elit</Text>
-          <Text>sed do eiusmod tempor incididunt ut labore</Text>
-        </Box>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        Art, Music,{' '}
+        Drawing
+        , Playing Guitar,{' '}
+        Photography
+        , Leica, Machine Learning
       </Section>
+
+
     </Container >
   )
 }
